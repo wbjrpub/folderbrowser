820d451fd5aa639ec4b44e17ae9d50cb573e0319
Wouter Batelaan <wbjrpub@gmail.com>
2021-01-15 21:15:34 +0000

    add pre-commit config

--- folderserver/__main__.py
+++ folderserver/__main__.py
@@ -4,13 +4,13 @@ from time import sleep
 
 from .legacy import Server
 
-log = logging.getLogger('folderserver.main')
+log = logging.getLogger("folderserver.main")
 log.setLevel(logging.DEBUG)
 
 # create console handler and set level to info
 ch = logging.StreamHandler()
 ch.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s')
+formatter = logging.Formatter("%(asctime)s %(levelname)s: %(message)s")
 ch.setFormatter(formatter)
 log.addHandler(ch)
 
--- folderserver/legacy.py
+++ folderserver/legacy.py
@@ -3,16 +3,20 @@ import html, urllib.request, urllib.parse, urllib.error
 import urllib.parse
 from io import StringIO, BytesIO
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 from http.server import HTTPServer
 from http.server import SimpleHTTPRequestHandler
 
+
 class RequestHandler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        self.routes = [(re.compile(rx), method) for rx, method in (
-            ('^/browse(?P<path>.*)', self.__browse),
-            # ('^/(?P<session_id>\d+)/browse(?P<path>.*)', self.__browse),
-        )]
+        self.routes = [
+            (re.compile(rx), method)
+            for rx, method in (
+                ("^/browse(?P<path>.*)", self.__browse),
+                # ('^/(?P<session_id>\d+)/browse(?P<path>.*)', self.__browse),
+            )
+        ]
         SimpleHTTPRequestHandler.__init__(self, *args, **kwargs)
 
     CSS = """
@@ -37,14 +41,14 @@ table, th, td {
 """
 
     @staticmethod
-    def sizeof_fmt(num, suffix='B'):
+    def sizeof_fmt(num, suffix="B"):
         if num < 1024:
             return "%dB" % num
-        for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
             if abs(num) < 1024.0:
                 return "%3.1f%s%s" % (num, unit, suffix)
             num /= 1024.0
-        return "%.1f%s%s" % (num, 'Yi', suffix)
+        return "%.1f%s%s" % (num, "Yi", suffix)
 
     def do_GET(self):
         for rx, func in self.routes:
@@ -58,26 +62,32 @@ table, th, td {
         return self.__default_route()
 
     def __browse(self, groupdict):
-        self.path = groupdict['path']
-        if self.path == '' or os.path.isdir(self.path):
-            if not self.path.endswith('/'):
-                self.path += '/'
+        self.path = groupdict["path"]
+        if self.path == "" or os.path.isdir(self.path):
+            if not self.path.endswith("/"):
+                self.path += "/"
         action = reverse = None
-        if '?' in self.path:
-            self.path, query_params = self.path.split('?', 1)
+        if "?" in self.path:
+            self.path, query_params = self.path.split("?", 1)
             query_params = urllib.parse.parse_qs(query_params)
-            if 'tail' in query_params:
-                action = 'tail'
-                reverse = 'head'
-            elif 'head' in query_params:
-                action = 'head'
-                reverse = 'tail'
+            if "tail" in query_params:
+                action = "tail"
+                reverse = "head"
+            elif "head" in query_params:
+                action = "head"
+                reverse = "tail"
             else:
                 self.send_error(404, "No such action")
                 return
-        self.displaypath = "Toplevel" if self.path=='/' else html.escape(urllib.parse.unquote(self.path.strip('/')))
+        self.displaypath = (
+            "Toplevel"
+            if self.path == "/"
+            else html.escape(urllib.parse.unquote(self.path.strip("/")))
+        )
         if action:
-            self.translated_path = SimpleHTTPRequestHandler.translate_path(self, self.path)
+            self.translated_path = SimpleHTTPRequestHandler.translate_path(
+                self, self.path
+            )
             if os.path.isfile(self.translated_path):
                 try:
                     n = int(query_params[action][0])
@@ -104,13 +114,21 @@ table, th, td {
         entries.sort(key=lambda a: a.lower())
         f = StringIO()
         f.write('<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">')
-        f.write("<html>\n<head>\n<title>Directory Listing for %s</title>\n" % self.displaypath)
+        f.write(
+            "<html>\n<head>\n<title>Directory Listing for %s</title>\n"
+            % self.displaypath
+        )
         f.write(self.CSS)
-        f.write("</head><body>\n<div id='header'><h2>Directory Listing for %s</h2>\n" % self.displaypath)
+        f.write(
+            "</head><body>\n<div id='header'><h2>Directory Listing for %s</h2>\n"
+            % self.displaypath
+        )
 
         f.write("</div><hr>\n<table id='dirlisting'>\n")
-        if self.path != '/':
-            f.write('<tr><td>&nbsp;</td><td colspan=3><a href="..">[Parent Directory]</a></td></tr>\n')
+        if self.path != "/":
+            f.write(
+                '<tr><td>&nbsp;</td><td colspan=3><a href="..">[Parent Directory]</a></td></tr>\n'
+            )
         dirs = []
         files = []
         for name in entries:
@@ -129,24 +147,36 @@ table, th, td {
                 statinfo = os.stat(fullname)
                 size = self.sizeof_fmt(statinfo.st_size)
                 n = 40
-                files.append("""<tr>
+                files.append(
+                    """<tr>
   <td align=right>%s</td>
   <td><a href="%s">%s</a></td>
   <td><a href="%s?head=%d" title="First %d lines">[head]</a></td>
   <td><a href="%s?tail=%d" title="Last %d lines">[tail]</a></td>
 </tr>\n"""
-                    % (size,
-                       linkname_quoted, html.escape(displayname),
-                       linkname_quoted, n, n,
-                       linkname_quoted, n, n,
-                    ))
+                    % (
+                        size,
+                        linkname_quoted,
+                        html.escape(displayname),
+                        linkname_quoted,
+                        n,
+                        n,
+                        linkname_quoted,
+                        n,
+                        n,
+                    )
+                )
             else:
-                dirs.append("""<tr>
+                dirs.append(
+                    """<tr>
   <td align=right>Directory</td>
   <td colspan='3'><a href="%s">%s</a></td>
 </tr>\n"""
-                    % (linkname_quoted, html.escape(displayname),
-                    ))
+                    % (
+                        linkname_quoted,
+                        html.escape(displayname),
+                    )
+                )
         for s in dirs:
             f.write(s)
         for s in files:
@@ -162,9 +192,10 @@ table, th, td {
         return BytesIO(f.read().encode())
 
     def filepart(self, action, reverse, n=40):
-        linkname = urllib.parse.quote(os.path.basename(self.path.strip('/')))
+        linkname = urllib.parse.quote(os.path.basename(self.path.strip("/")))
         f = StringIO()
-        f.write("""<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+        f.write(
+            """<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <html>
 <head>
   <title>%s</title>
@@ -182,18 +213,39 @@ table, th, td {
 <a href="%s">[Directory]</a></p>
 </div>
 <hr/><pre>
-""" % (html.escape(self.translated_path),
-       self.CSS,
-       self.displaypath, #html.escape(self.translated_path),
-       linkname, action, max(1, int(n/2)), action, max(1, int(n/2)),
-       linkname, action, n, action, n,
-       linkname, action, max(2, int(n*2)), action, max(2, int(n*2)),
-       linkname, reverse, n, reverse, n,
-       linkname,
-       '.' #'browse/%s' % urllib.quote(dirname),
-    ))
+"""
+            % (
+                html.escape(self.translated_path),
+                self.CSS,
+                self.displaypath,  # html.escape(self.translated_path),
+                linkname,
+                action,
+                max(1, int(n / 2)),
+                action,
+                max(1, int(n / 2)),
+                linkname,
+                action,
+                n,
+                action,
+                n,
+                linkname,
+                action,
+                max(2, int(n * 2)),
+                action,
+                max(2, int(n * 2)),
+                linkname,
+                reverse,
+                n,
+                reverse,
+                n,
+                linkname,
+                ".",  #'browse/%s' % urllib.quote(dirname),
+            )
+        )
         try:
-            s = subprocess.check_output([action, '-n', str(n), self.translated_path], encoding='utf-8')
+            s = subprocess.check_output(
+                [action, "-n", str(n), self.translated_path], encoding="utf-8"
+            )
             f.write(html.escape(s))
         except Exception as ex:
             f.write(str(ex))
@@ -208,27 +260,27 @@ table, th, td {
         self.wfile.write(f.read().encode())
 
     def __default_route(self):
-        resp = ''
-        resp += 'URL was: %s</br>' % self.path
+        resp = ""
+        resp += "URL was: %s</br>" % self.path
         resp += '<a href="/browse/">browse</a> '
         # resp += '<a href="/%d/browse/">browse</a> ' % self.session_id
         self.send_response(200)
-        self.send_header('content-type', 'text/html')
+        self.send_header("content-type", "text/html")
         self.end_headers()
         self.wfile.write(str.encode(resp))
 
 
-
 class Server:
-    def __init__(self, log: logging.Logger):
-        self.log = log
-        self.webserver_port = 8081
-        self.webserver = HTTPServer(('', self.webserver_port), RequestHandler)
-        # self.webserver = HTTPServer(('', self.webserver_port), partial(RequestHandler, self))
-        self.webserver.allow_reuse_address = True
-        self.HOSTNAME = '127.0.0.1'
-        self.url = 'http://%s:%d/' % (self.HOSTNAME, self.webserver_port)
-        self.thread = threading.Thread(name='webserver', target=self.webserver.serve_forever)
-        self.thread.setDaemon(False)
-        self.thread.start()
-        log.info("Server URL: %s", self.url)
+    self.log = log
+    self.webserver_port = 8081
+    self.webserver = HTTPServer(("", self.webserver_port), RequestHandler)
+    # self.webserver = HTTPServer(('', self.webserver_port), partial(RequestHandler, self))
+    self.webserver.allow_reuse_address = True
+    self.HOSTNAME = "127.0.0.1"
+    self.url = "http://%s:%d/" % (self.HOSTNAME, self.webserver_port)
+    self.thread = threading.Thread(
+        name="webserver", target=self.webserver.serve_forever
+    )
+    self.thread.setDaemon(False)
+    self.thread.start()
+    log.info("Server URL: %s", self.url)
